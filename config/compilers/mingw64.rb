2bfb440713b4b4a2071a78b2c8cccca0bc8052f9
Tsuyoshi Morita <golirev@gmail.com>
2016-07-30 14:09:19 +0900

    Delete unnecessary files for msys2 devkit environment

--- config/compilers/mingw64.rb
+++ config/compilers/mingw64.rb
@@ -6,32 +6,12 @@ module DevKitInstaller
       :programs => [ :gcc, :cpp, :'g++' ],
       :program_prefix => nil,
       :url_1 => 'http://downloads.sourceforge.net/mingw-w64-dgn',
-      :url_2 => 'http://downloads.sourceforge.net/mingw',
-      :url_3 => 'http://downloads.sourceforge.net/gnuwin32',
       :target => 'sandbox/devkit/mingw',
       :relocate => 'sandbox/devkit/mingw/mingw32',
       :knap_path => '610-drangon',
       :files => {
         :url_1 => [
           'mingw-w32-bin-i686-20160428.7z'
-        ],
-        :url_2 => [
-          'autoconf2.1-2.13-4-mingw32-bin.tar.lzma',
-          'autoconf2.5-2.68-1-mingw32-bin.tar.lzma',
-          'autoconf-10-1-mingw32-bin.tar.lzma',
-          'automake1.11-1.11.1-1-mingw32-bin.tar.lzma',
-          'automake-4-1-mingw32-bin.tar.lzma',
-          'libexpat-2.0.1-1-mingw32-dll-1.tar.gz',
-          'libtool-2.4-1-mingw32-bin.tar.lzma',
-          'bsdtar-2.8.3-1-mingw32-bin.tar.bz2',
-          'bsdcpio-2.8.3-1-mingw32-bin.tar.bz2',
-          'libarchive-2.8.3-1-mingw32-dll-2.tar.bz2',
-          'libbz2-1.0.5-2-mingw32-dll-2.tar.gz',
-          'liblzma-4.999.9beta_20100401-1-mingw32-dll-1.tar.bz2',
-          'libz-1.2.3-1-mingw32-dll-1.tar.gz'
-        ],
-        :url_3 => [
-          'which-2.20-bin.zip'
         ],
       }
     )
@@ -42,8 +22,6 @@ module DevKitInstaller
       :programs => [ :gcc, :cpp, :'g++' ],
       :program_prefix => nil,
       :url_1 => 'http://downloads.sourceforge.net/mingw-w64-dgn',
-      :url_2 => 'http://downloads.sourceforge.net/mingw',
-      :url_3 => 'http://downloads.sourceforge.net/gnuwin32',
       :target => 'sandbox/devkit/mingw',
       :relocate => 'sandbox/devkit/mingw/mingw64',
       :host => 'x86_64-w64-mingw32',
@@ -51,24 +29,6 @@ module DevKitInstaller
       :files => {
         :url_1 => [
           'mingw-w64-bin-x86_64-20160428.7z'
-        ],
-        :url_2 => [
-          'autoconf2.1-2.13-4-mingw32-bin.tar.lzma',
-          'autoconf2.5-2.68-1-mingw32-bin.tar.lzma',
-          'autoconf-10-1-mingw32-bin.tar.lzma',
-          'automake1.11-1.11.1-1-mingw32-bin.tar.lzma',
-          'automake-4-1-mingw32-bin.tar.lzma',
-          'libexpat-2.0.1-1-mingw32-dll-1.tar.gz',
-          'libtool-2.4-1-mingw32-bin.tar.lzma',
-          'bsdtar-2.8.3-1-mingw32-bin.tar.bz2',
-          'bsdcpio-2.8.3-1-mingw32-bin.tar.bz2',
-          'libarchive-2.8.3-1-mingw32-dll-2.tar.bz2',
-          'libbz2-1.0.5-2-mingw32-dll-2.tar.gz',
-          'liblzma-4.999.9beta_20100401-1-mingw32-dll-1.tar.bz2',
-          'libz-1.2.3-1-mingw32-dll-1.tar.gz'
-        ],
-        :url_3 => [
-          'which-2.20-bin.zip'
         ],
       }
     )
@@ -79,32 +39,12 @@ module DevKitInstaller
       :programs => [ :gcc, :cpp, :'g++' ],
       :program_prefix => nil,
       :url_1 => 'http://downloads.sourceforge.net/mingw-w64',
-      :url_2 => 'http://downloads.sourceforge.net/mingw',
-      :url_3 => 'http://downloads.sourceforge.net/gnuwin32',
       :target => 'sandbox/devkit/mingw',
       :relocate => 'sandbox/devkit/mingw/mingw32',
       :knap_path => '530-win32-sjlj',
       :files => {
         :url_1 => [
           'i686-5.3.0-release-win32-sjlj-rt_v4-rev0.7z'
-        ],
-        :url_2 => [
-          'autoconf2.1-2.13-4-mingw32-bin.tar.lzma',
-          'autoconf2.5-2.68-1-mingw32-bin.tar.lzma',
-          'autoconf-10-1-mingw32-bin.tar.lzma',
-          'automake1.11-1.11.1-1-mingw32-bin.tar.lzma',
-          'automake-4-1-mingw32-bin.tar.lzma',
-          'libexpat-2.0.1-1-mingw32-dll-1.tar.gz',
-          'libtool-2.4-1-mingw32-bin.tar.lzma',
-          'bsdtar-2.8.3-1-mingw32-bin.tar.bz2',
-          'bsdcpio-2.8.3-1-mingw32-bin.tar.bz2',
-          'libarchive-2.8.3-1-mingw32-dll-2.tar.bz2',
-          'libbz2-1.0.5-2-mingw32-dll-2.tar.gz',
-          'liblzma-4.999.9beta_20100401-1-mingw32-dll-1.tar.bz2',
-          'libz-1.2.3-1-mingw32-dll-1.tar.gz'
-        ],
-        :url_3 => [
-          'which-2.20-bin.zip'
         ],
       }
     )
@@ -122,24 +62,6 @@ module DevKitInstaller
       :files => {
         :url_1 => [
           'x86_64-5.3.0-release-win32-seh-rt_v4-rev0.7z'
-        ],
-        :url_2 => [
-          'autoconf2.1-2.13-4-mingw32-bin.tar.lzma',
-          'autoconf2.5-2.68-1-mingw32-bin.tar.lzma',
-          'autoconf-10-1-mingw32-bin.tar.lzma',
-          'automake1.11-1.11.1-1-mingw32-bin.tar.lzma',
-          'automake-4-1-mingw32-bin.tar.lzma',
-          'libexpat-2.0.1-1-mingw32-dll-1.tar.gz',
-          'libtool-2.4-1-mingw32-bin.tar.lzma',
-          'bsdtar-2.8.3-1-mingw32-bin.tar.bz2',
-          'bsdcpio-2.8.3-1-mingw32-bin.tar.bz2',
-          'libarchive-2.8.3-1-mingw32-dll-2.tar.bz2',
-          'libbz2-1.0.5-2-mingw32-dll-2.tar.gz',
-          'liblzma-4.999.9beta_20100401-1-mingw32-dll-1.tar.bz2',
-          'libz-1.2.3-1-mingw32-dll-1.tar.gz'
-        ],
-        :url_3 => [
-          'which-2.20-bin.zip'
         ],
       }
     )
@@ -166,8 +88,6 @@ module DevKitInstaller
       :programs => [ :gcc, :cpp, :'g++' ],
       :program_prefix => nil,
       :url_1 => 'http://downloads.sourceforge.net/mingw-w64',
-      :url_2 => 'http://downloads.sourceforge.net/mingw',
-      :url_3 => 'http://downloads.sourceforge.net/gnuwin32',
       :target => 'sandbox/devkit/mingw',
       :relocate => 'sandbox/devkit/mingw/mingw64',
       :host => 'x86_64-w64-mingw32',
