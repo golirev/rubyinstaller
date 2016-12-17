730f983cfc9b1bb0ac1a65123dbd0b6fdc4a4dea
Tsuyoshi Morita <golirev@gmail.com>
2016-12-17 18:04:27 +0900

    Update msys2 Dec-11

--- config/devkit.rb
+++ config/devkit.rb
@@ -33,24 +33,24 @@ module DevKitInstaller
     :target => 'sandbox/devkit',
     :files => {
       :url_1 => [
-        'msys2-runtime-2.5.2-2-i686.pkg.tar.xz',
+        'msys2-runtime-2.6.0-1-i686.pkg.tar.xz',
         'filesystem-2016.07-2-i686.pkg.tar.xz',
-        'msys2-launcher-git-0.3.29.4028b6c-1-i686.pkg.tar.xz',
+        'msys2-launcher-git-0.3.32.56c2ba7-2-i686.pkg.tar.xz',
         'rebase-4.4.2-1-i686.pkg.tar.xz',
         'libiconv-1.14-2-i686.pkg.tar.xz',
         'libintl-0.19.7-3-i686.pkg.tar.xz',
-        'ncurses-6.0.20160220-1-i686.pkg.tar.xz',
+        'ncurses-6.0.20161112-1-i686.pkg.tar.xz',
         'libreadline-6.3.008-7-i686.pkg.tar.xz',
         'gcc-libs-5.3.0-3-i686.pkg.tar.xz',
         'gmp-6.1.0-2-i686.pkg.tar.xz',
         'bash-4.3.046-1-i686.pkg.tar.xz',
         'coreutils-8.25-1-i686.pkg.tar.xz',
-        'grep-2.22-3-i686.pkg.tar.xz',
+        'grep-2.26-1-i686.pkg.tar.xz',
         'libpcre-8.38-1-i686.pkg.tar.xz',
-        'gawk-4.1.3-1-i686.pkg.tar.xz',
+        'gawk-4.1.4-1-i686.pkg.tar.xz',
         'mpfr-3.1.4-1-i686.pkg.tar.xz',
         'sed-4.2.2-2-i686.pkg.tar.xz',
-        'diffutils-3.3-3-i686.pkg.tar.xz',
+        'diffutils-3.5-1-i686.pkg.tar.xz',
         'bison-3.0.4-1-i686.pkg.tar.xz',
         'm4-1.4.17-4-i686.pkg.tar.xz',
         'make-4.2.1-1-i686.pkg.tar.xz',
@@ -63,25 +63,25 @@ module DevKitInstaller
         'less-481-1-i686.pkg.tar.xz',
         'findutils-4.6.0-1-i686.pkg.tar.xz',
         'perl-5.22.1-1-i686.pkg.tar.xz',
-        'mintty-1~2.4.0-1-i686.pkg.tar.xz',
+        'mintty-1~2.6.2-1-i686.pkg.tar.xz',
         'autoconf-2.69-3-any.pkg.tar.xz',
         'automake1.15-1.15-2-any.pkg.tar.xz',
         'automake-wrapper-10-1-any.pkg.tar.xz',
-        'libexpat-2.1.1-1-i686.pkg.tar.xz',
+        'libexpat-2.2.0-2-i686.pkg.tar.xz',
         'libtool-2.4.6-2-i686.pkg.tar.xz',
-        'bsdtar-3.2.0-1-i686.pkg.tar.xz',
-        'bsdcpio-3.2.0-1-i686.pkg.tar.xz',
-        'libarchive-3.2.0-1-i686.pkg.tar.xz',
+        'bsdtar-3.2.2-2-i686.pkg.tar.xz',
+        'bsdcpio-3.2.2-2-i686.pkg.tar.xz',
+        'libarchive-3.2.2-2-i686.pkg.tar.xz',
         'libbz2-1.0.6-2-i686.pkg.tar.xz',
         'liblzma-5.2.2-1-i686.pkg.tar.xz',
         'liblzo2-2.09-1-i686.pkg.tar.xz',
-        'libnettle-3.2-1-i686.pkg.tar.xz',
+        'libnettle-3.3-1-i686.pkg.tar.xz',
         'libxml2-2.9.2-2-i686.pkg.tar.xz',
         'zlib-1.2.8-3-i686.pkg.tar.xz',
         'which-2.21-2-i686.pkg.tar.xz',
         'patch-2.7.5-1-i686.pkg.tar.xz',
         'tar-1.29-1-i686.pkg.tar.xz',
-        'gzip-1.7-1-i686.pkg.tar.xz',
+        'gzip-1.8-1-i686.pkg.tar.xz',
         'bzip2-1.0.6-2-i686.pkg.tar.xz',
         'xz-5.2.2-1-i686.pkg.tar.xz'
       ]
@@ -90,14 +90,14 @@ module DevKitInstaller
 
   if ENV['MSYS_ADD']
     MSYS.files[:url_1] = MSYS.files[:url_1] + [
-      'git-2.9.1-1-i686.pkg.tar.xz',
-      'libcurl-7.49.1-1-i686.pkg.tar.xz',
-      'libopenssl-1.0.2.h-1-i686.pkg.tar.xz',
+      'git-2.10.2-1-i686.pkg.tar.xz',
+      'libcurl-7.51.0-1-i686.pkg.tar.xz',
+      'libopenssl-1.0.2.j-1-i686.pkg.tar.xz',
       # add for knap-build
       'automake1.11-1.11.6-3-any.pkg.tar.xz',
       # dash added for autorebase.bat execution
-      'dash-0.5.8-1-i686.pkg.tar.xz',
-      'vim-7.4.1721-1-i686.pkg.tar.xz'
+      'dash-0.5.9.1-1-i686.pkg.tar.xz',
+      'vim-8.0.0124-1-i686.pkg.tar.xz'
     ]
   end
 
